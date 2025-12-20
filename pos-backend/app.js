ff6a85ed1c43d0bf052abaaee6cff13a23866a04
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-12-20 16:36:09 +0700

    connection with mongodb atlas

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the POS Backend" });
