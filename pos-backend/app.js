2109f690b84cb5f46d7a0748c95863ec7c64373d
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-12-25 11:27:40 +0700

    install cors, tanstack, axios and notistack

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -6,14 +6,21 @@ const config = require("./config/config");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
 const cookieParser = require("cookie-parser");
 const app = express();
+const cors = require("cors");
 
 const PORT = config.port;
 connectDB();
 
+//Enable CORS
+app.use(cors({
+  credentials:true, // Allow cookies to be sent in cross-origin requests
+  origin:['http://localhost:5173']
+}));
 //Middleware to parse JSON requests
 app.use(express.json());
 //Middleware to parse cookies
 app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the POS Backend" });
